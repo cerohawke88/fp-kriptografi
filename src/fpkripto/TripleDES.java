6c085b6bacf62ee0fd8f7d19106331af768860cf
Vicky Sultan <vickysultan08@gmail.com>
2019-05-08 11:14:13 +0700

    Initial commit

---
+++ src/fpkripto/TripleDES.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fpkripto;
+
+import java.security.spec.KeySpec;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+import java.util.Scanner;
+
+/**
+ *
+ * @author VICKY
+ */
+public class TripleDES {
+    
+    private static final String UNICODE_FORMAT = "UTF8";
+    public static final String DESEDE_ENCRYPTION_SCHEME = "TripleDES";
+    private KeySpec myKeySpec;
+    private SecretKeyFactory mySecretKeyFactory;
+    private Cipher cipher;
+    byte[] keyAsBytes;
+    private String myEncryptionKey;
+    private String myEncryptionScheme;
+    Scanner input = new Scanner(System.in);
+    SecretKey key;
+    
+    public TripleDES() throws Exception
+    {
+        System.out.print("Key: ");
+        String keyText = input.nextLine();
+        myEncryptionKey = keyText;
+        myEncryptionScheme = DESEDE_ENCRYPTION_SCHEME;
+        keyAsBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
+        myKeySpec = new DESedeKeySpec(keyAsBytes);
+        mySecretKeyFactory = SecretKeyFactory.getInstance(myEncryptionScheme);
+        cipher = Cipher.getInstance(myEncryptionScheme);
+        key = mySecretKeyFactory.generateSecret(myKeySpec);
+    }
+ 
+    /**
+     * Method To Encrypt The String
+     */
+    public String encrypt(String unencryptedString) {
+        String encryptedString = null;
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            BASE64Encoder base64encoder = new BASE64Encoder();
+            encryptedString = base64encoder.encode(encryptedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedString;
+    }
+    /**
+     * Method To Decrypt An Encrypted String
+     */
+    public String decrypt(String encryptedString) {
+        String decryptedText=null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            BASE64Decoder base64decoder = new BASE64Decoder();
+            byte[] encryptedText = base64decoder.decodeBuffer(encryptedString);
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText= bytes2String(plainText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedText;
+    }
+    /**
+     * Returns String From An Array Of Bytes
+     */
+    private static String bytes2String(byte[] bytes) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < bytes.length; i++) {
+            stringBuffer.append((char) bytes[i]);
+        }
+        return stringBuffer.toString();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        TripleDES myEncryptor= new TripleDES();
+        
+        Scanner input = new Scanner(System.in);
+ 
+        System.out.print("String To Encrypt: ");
+        String encrypt = input.nextLine();
+        String stringToEncrypt=(encrypt);
+        String encrypted=myEncryptor.encrypt(stringToEncrypt);
+        
+        String decrypted=myEncryptor.decrypt(encrypted);
+
+        
+        System.out.println("Encrypted Value : " + encrypted);
+        System.out.println("Decrypted Value : "+decrypted);
+    }
+    
+}
